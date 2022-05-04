a70312dd43ad0cec2f8a0cd434cc8bc53d533bc2
EddySupra <Eduardo23@csu.fullerton.edu>
2022-05-04 16:43:56 -0700

    done

--- main.cpp
+++ main.cpp
@@ -1,1 +1,10 @@
+#include <iostream>
 
+int main()
+{
+    int a, b;
+    std::cin >> a >> b;
+    int c = a + b;
+    std::cout << "Hello World" << std::endl;
+    std::cout << a << " + " << b << " = " << c << std::endl;
+}
--- main.scm
+++ main.scm
@@ -1,1 +1,8 @@
+(display "Hello World")
+(newline)
+(define (sum a b)
+(display "2 + 5 = ")
+(display (+ a b)))
 
+(sum 2 5)
+(newline)
